cb750f658b8022ff3ddf609c321a4136dcce3cd3
Killian <killian.hd@gmail.com>
2024-04-25 00:48:19 +0200

    Adding map image and buttons

--- script.js
+++ script.js
@@ -3,5 +3,29 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     licenseKey: 'gplv3-license',
-    verticalCentered: true,
+    slideNavigation: true,
+    navigationTooltips: ['Home', 'About', 'Projects', 'Contact'],
 });
+
+// Select all buttons with the class "stadium-button"
+const buttons = d3.selectAll(".stadium-button");
+
+// Add click event listener to each button
+buttons.on("click", function() {
+    // Remove "clicked" class from all buttons to ensure one button is clicked at a time
+    buttons.classed("clicked", false);
+    
+    // Add "clicked" class to the clicked button
+    d3.select(this).classed("clicked", true);
+    
+    let value = d3.select(this).text();
+    console.log("Button value:", value);
+});
+
+// Get the slider element
+const slider = d3.select("sliderMap");
+
+// Output the current value when the slider value changes
+slider.oninput = function() {
+    console.log(this.value);
+};
