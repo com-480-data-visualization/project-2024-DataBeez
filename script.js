39c75609f5387b650dc705d27815e1efb78687a0
Killian <killian.hd@gmail.com>
2024-04-26 15:57:44 +0200

    Final Skeleton Milestone 2

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     licenseKey: 'gplv3-license',
-    slideNavigation: true,
-    navigationTooltips: ['Home', 'About', 'Projects', 'Contact'],
+    sectionsColor: ['#f2f2f2', '#ffcbc4', '#ffcbc4', '#c4d9ff', '#c4d9ff', '#c5ffc4', '#c5ffc4', '#ddc4ff', '#ddc4ff', '#f2f2f2'], 
 });
 
 // Select all buttons with the class "stadium-button"
@@ -12,14 +11,26 @@ const buttons = d3.selectAll(".stadium-button");
 
 // Add click event listener to each button
 buttons.on("click", function() {
+
     // Remove "clicked" class from all buttons to ensure one button is clicked at a time
     buttons.classed("clicked", false);
     
     // Add "clicked" class to the clicked button
     d3.select(this).classed("clicked", true);
     
+    // Get the value of the clicked button
     let value = d3.select(this).text();
-    console.log("Button value:", value);
+
+    // Select the image element
+    const img = d3.select('#map');
+
+    // Change the image source based on the value of the clicked button
+    if (value === "Points") {
+        img.attr("src", "img/points_map.jpg");
+    }
+    else if (value === "Heatmap") {
+        img.attr("src", "img/heatmap_map.jpg");
+    }
 });
 
 // Get the slider element
